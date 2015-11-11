8518f17c0ec77c167dbd66e00ad13c041ec41ce5
sravankumar <s521731@mail.nwmissouri.edu>
2015-11-11 17:22:31 -0600

    player movement mechanics
    basic movement added

---
+++ CellSquadronMechanics/ProjectSettings/Assets/PlayerBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+    public float currentSpeed = 10.0f;
+    private Rigidbody body;
+	// Use this for initialization
+	void Start () {
+        body = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        body.AddTorque(Input.GetAxis("Vertical") * transform.right);
+        body.AddTorque(Input.GetAxis("Horizontal") * transform.forward * -1f);
+        if (Input.GetKey(KeyCode.Space)) { transform.Translate(Vector3.forward * Time.deltaTime * currentSpeed); }
+        
+
+    }
+}
---
+++ CellSquadronMechanics/ProjectSettings/Assets/cameraBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraBehaviour : MonoBehaviour {
+
+    public Ray ray;
+    public Vector2 mousePos;
+
+    private Transform target;
+    private GameObject player;
+    private Vector3 wantedPosition;
+
+    public float distance = 50.0f;
+    public float height = 3.0f;
+    public float damping = 5.0f;
+    public float rotationDamping = 0.5f;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        target = player.transform;
+    }
+
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(mousePos);
+    }
+
+    void LateUpdate()
+    {
+        SmoothFollow();
+    }
+
+    void SmoothFollow()
+    {
+        //Follow player smoothly
+        wantedPosition = target.TransformPoint(0, height, -distance);
+        transform.position = Vector3.Lerp(transform.position, wantedPosition, Time.deltaTime * damping);
+
+        Quaternion wantedRotation = Quaternion.LookRotation(target.position - transform.position, target.up);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, wantedRotation, Time.deltaTime * rotationDamping);
+
+        transform.LookAt(target, target.up);
+
+
+
+
+    }
+   
+
+}
