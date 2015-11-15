7c01e979efb3a15e98e2f6019b44b0f4bbb3adf4
sravankumar <s521731@mail.nwmissouri.edu>
2015-11-14 19:19:16 -0600

    added joystick

--- CellSquadronMechanics/ProjectSettings/Assets/PlayerBehaviour.cs
+++ CellSquadronMechanics/ProjectSettings/Assets/PlayerBehaviour.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using CnControls;
 
 public class PlayerBehaviour : MonoBehaviour {
     public float currentSpeed = 10.0f;
     private Rigidbody body;
+    private Vector3 movement;
+    public float sentivity = 20.0f;
 	// Use this for initialization
 	void Start () {
         body = gameObject.GetComponent<Rigidbody>();
@@ -12,10 +15,13 @@ public class PlayerBehaviour : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        body.AddTorque(Input.GetAxis("Vertical") * transform.right);
-        body.AddTorque(Input.GetAxis("Horizontal") * transform.forward * -1f);
-        if (Input.GetKey(KeyCode.Space)) { transform.Translate(Vector3.forward * Time.deltaTime * currentSpeed); }
-        
+        //body.AddTorque(CnInputManager.GetAxis("Vertical") * transform.right);
+        //body.AddTorque(CnInputManager.GetAxis("Horizontal") * transform.forward * -1f);
+         transform.Translate(Vector3.forward * Time.deltaTime * currentSpeed);
+        movement = new Vector3(CnInputManager.GetAxis("Horizontal")*sentivity*-1.0f, CnInputManager.GetAxis("Vertical")*sentivity, 0.0f);
+        body.velocity = movement;
 
     }
+   
+
 }
